70c4b166e34cf2665c52e67a6db01d2c3799e7b1
Mitiandrianasoa <mitiandrianasoa@gmail.com>
2025-10-17 22:02:17 +0100

    correction

--- src/main/java/com/sprint/FrontServlet.java
+++ src/main/java/com/sprint/FrontServlet.java
@@ -1,17 +1,18 @@
 package com.sprint;
 
 import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.RequestDispatcher;
 
 public class FrontServlet extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        charResource(req, resp);
+        RechercheResource(req, resp);
     }
     
     private void RechercheResource(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
