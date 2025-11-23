d1c3a5b41d4e571a1c4a9fc2a9b7722adc2d563e
Mitiandrianasoa <mitiandrianasoa@gmail.com>
2025-11-23 17:16:38 +0300

    sprint5

--- src/main/java/com/sprint/model/ModelView.java
+++ src/main/java/com/sprint/model/ModelView.java
@@ -1,10 +1,15 @@
 package com.sprint.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     private String view;
+    private Map<String, Object> data;
 
     public ModelView(String view) {
         this.view = view;
+        this.data = new HashMap<>();
     }
 
     public String getView() {
@@ -13,5 +18,18 @@ public class ModelView {
 
     public void setView(String view) {
         this.view = view;
+    }
+
+    public ModelView addObject(String key, Object value) {
+        this.data.put(key, value);
+        return this;
+    }
+
+    public Map<String, Object> getData() {
+        return this.data;
+    }
+
+    public Object getObject(String key) {
+        return this.data.get(key);
     }
 }
